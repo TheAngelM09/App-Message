908b81c4b147ea103e7fd7a69b61153c3e76f0ba
TheAngelM09 <165745626+TheAngelM09@users.noreply.github.com>
2025-08-27 09:51:02 -0400

    Feat: Demo reception of WhatsApp messages

--- src/routes/MessageRoutes.py
+++ src/routes/MessageRoutes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import JWTManager, jwt_required
 from src.database.redisConnect import connect
+from decouple import config
 
 main = Blueprint('message_blueprint', __name__)
 
@@ -19,3 +20,21 @@ def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
 def home():
     return jsonify('Views Message')
 
+@main.route('/ws', methods=['GET','POST'])
+def msg_ws():
+
+    if request.method == "GET":
+        if request.args.get('hub.verify_token') == config('WS_TOKEN'):
+            return request.args.get('hub.challenge')
+        else:
+            return jsonify({"status": "success"}, 200)
+
+    if request.method == 'POST':
+        data = request.json
+        message = {
+            'id': data["entry"][0]["changes"][0]["value"]["messages"][0]["id"],
+            'name': data["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"],
+            'msj': data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"],
+            'telf': data["entry"][0]["changes"][0]["value"]["messages"][0]["from"],
+        }
+        return jsonify(message)
